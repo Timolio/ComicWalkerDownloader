9f6bc6aabc23a2fae1813e871ee3227fa133a553
Timolio <94037690+Timolio@users.noreply.github.com>
2025-05-09 19:39:29 +0200

    chore: remove build directory and add to .gitignore

--- build/lib/comicwalker_downloader/utils.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def is_valid_url(url: str) -> bool:
-    pattern = r'^(https?://)?comic-walker\.com/detail/KC_\d+_S(/episodes/KC_\d+_E)?(?:\?.*)?$'
-
-    if re.match(pattern, url):
-        return True
-    else:
-        return False
